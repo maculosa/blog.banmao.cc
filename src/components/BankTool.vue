8ee7201f175fdb3a1b029e2f0df9b8a0e820620e
Qingfeng Liu <maculosa@163.com>
2024-08-16 15:46:58 +0800

    feat(BankTool): 支持多数据合计，和上次历史记录显示

--- src/components/BankTool.vue
+++ src/components/BankTool.vue
@@ -1,127 +1,234 @@
 <template>
-    <div class="flex gap-4">
-        <div class="w-full">
-            <form
-                class="mb-4 transition shadow hover:shadow-xl bg-#fff flex flex-col p-4 rounded-4">
+  <div class="flex gap-4">
+    <Card title="金额录入">
+      <template #header-extra>
+        <button
+          type="button"
+          class="cursor-pointer text-white transition-all text-14px mb-4 px-2 py-1 rounded-1 bg-blue-600 hover:bg-blue-500 hover:shadow"
+          @click="handleAdd">
+          新增
+        </button>
+      </template>
+      <div
+        class="flex items-center gap-2 mb-4"
+        v-for="(item, index) in items"
+        :key="item">
+        <label class="font-bold">金额 {{ index + 1 }}: </label>
+        <input
+          class="flex-1 h-32px p-16px border-1 rounded-1"
+          v-model="amount[index]"
+          type="text"
+          :placeholder="`金额 ${index + 1}`" />
+        <button
+          type="button"
+          class="flex justify-center items-center h-30px w-30px bg-red-100 text-red-5 rounded-4 hover:shadow"
+          @click="handleDelete(index)">
+          <div class="w-12px h-2px bg-red-600"></div>
+        </button>
+      </div>
 
-                <div class="flex flex-col gap-4">
-                    <label for="total" class="font-bold min-w-120px h-32px line-height-32px">Total：</label>
-                    <input class="h-32px p-16px border-1 rounded-1 mb-4" type="number" id="total"
-                        v-model="formModel.total" placeholder="Total" />
-                </div>
-                <div class="flex flex-col gap-4">
-                    <label for="total" class="font-bold min-w-120px h-32px line-height-32px">分隔数量：</label>
-                    <input class="h-32px p-16px border-1 rounded-1 mb-4" type="number" id="total"
-                        v-model="formModel.partsCount" placeholder="partsCount" />
-                </div>
-                <div class="flex flex-col gap-4">
-                    <label for="total" class="font-bold min-w-120px h-32px line-height-32px">浮动范围（%）：</label>
-                    <input class="h-32px p-16px border-1 rounded-1 mb-4" type="number" id="total"
-                        v-model="formModel.percentageRange" placeholder="partsCount" />
-                </div>
-                <div class="flex gap-4 justify-center">
-                    <button type="button"
-                        class="py-4px px-16px border-1 rounded-1 text-white transition bg-blue-600 hover:bg-blue-400 w-120px"
-                        @click="handleComputed">计算</button>
-                </div>
-            </form>
+      <div class="w-100% b-b-2 b-dashed my-4"></div>
+
+      <form class="transition p-4 rounded-4 grid gap-4">
+        <div class="flex items-center gap-4">
+          <label for="total" class="font-bold min-w-120px text-right"
+            >总金额：</label
+          >
+          <input
+            class="flex-1 h-32px p-16px border-1 rounded-1"
+            type="number"
+            id="total"
+            v-model="formModel.total"
+            placeholder="Total" />
         </div>
+        <div class="flex items-center gap-4">
+          <label for="total" class="font-bold min-w-120px text-right"
+            >分隔次数：</label
+          >
+          <input
+            class="flex-1 h-32px p-16px border-1 rounded-1"
+            type="number"
+            id="total"
+            v-model="formModel.partsCount"
+            placeholder="partsCount" />
+        </div>
+        <div class="flex items-center gap-4">
+          <label for="total" class="font-bold min-w-120px text-right"
+            >浮动范围（%）：</label
+          >
+          <input
+            class="flex-1 h-32px p-16px border-1 rounded-1"
+            type="number"
+            id="total"
+            v-model="formModel.percentageRange"
+            placeholder="partsCount" />
+        </div>
+        <div class="flex gap-4 justify-center">
+          <button
+            type="button"
+            class="w-100% block py-8px px-16px border-1 rounded-2 text-white transition bg-blue-600 hover:bg-blue-400"
+            @click="handleComputed">
+            计算
+          </button>
+        </div>
+      </form>
+    </Card>
 
-        <!-- <div>
-            <div v-for="(part, index) in parts" :key="index">
-                {{ part }}
-            </div>
-        </div> -->
-        <table border class="w-full bg-white">
-            <thead class="bg-gray-100">
-                <tr class="h-40px">
-                    <th class="w-120px text-center">序号</th>
-                    <th class="w-120px text-center">金额</th>
-                </tr>
-            </thead>
-            <tbody>
-                <template v-if="parts.length > 0">
-                    <tr class="h-40px" v-for="(part, index) in parts" :key="index"
-                        :style="{ backgroundColor: index % 2 !== 0 ? '#f5f5f5' : '#fff' }">
-                        <td class="text-center">{{ index + 1 }}</td>
-                        <td class="text-center">{{ part }}</td>
-                    </tr>
-                </template>
-                <tr v-else>
-                    <td colspan="2" class="text-center">
-                        暂无数据
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <Card title="计算结果">
+      <template #header-extra>
+        <button
+          type="button"
+          class="cursor-pointer text-white transition-all text-14px mb-4 px-2 py-1 rounded-1 bg-blue-600 hover:bg-blue-500 hover:shadow"
+          v-if="historyList.length > 0"
+          @click="checkedRecord = true">
+          上次记录
+        </button>
+      </template>
+      <table border class="w-full bg-white mt-4">
+        <thead class="bg-gray-100">
+          <tr class="h-40px">
+            <th class="w-120px text-center">序号</th>
+            <th class="w-120px text-center">金额</th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-if="record.length > 0">
+            <tr
+              class="h-40px"
+              v-for="(part, index) in record"
+              :key="index"
+              :style="{
+                backgroundColor: index % 2 !== 0 ? '#f5f5f5' : '#fff'
+              }">
+              <td class="text-center">{{ index + 1 }}</td>
+              <td class="text-center">{{ part }}</td>
+            </tr>
+          </template>
+          <tr v-else>
+            <td colspan="2" class="text-center">暂无数据</td>
+          </tr>
+        </tbody>
+      </table>
+    </Card>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue'
+  import { ref, reactive, computed, watch } from 'vue'
+  import Card from './Card/index.vue'
 
-defineOptions({
+  defineOptions({
     name: 'BankTool'
-})
+  })
 
-
-function randomSplit2Int(total, partsCount, percentageRange) {
+  function randomSplit2Int(total, partsCount, percentageRange) {
     if (partsCount <= 0) {
-        throw new Error("Parts count must be a positive integer");
+      throw new Error('Parts count must be a positive integer')
     }
     if (total < partsCount) {
-        throw new Error("Total must be greater than or equal to parts count");
+      throw new Error('Total must be greater than or equal to parts count')
     }
 
     // 计算平均值和波动范围
-    const avg = Math.floor(total / partsCount);
+    const avg = Math.floor(total / partsCount)
     // const range = total % partsCount;
-    const range = Math.floor(avg * percentageRange / 100);
-    const minPart = avg - range;
-    const maxPart = avg + range;
+    const range = Math.floor((avg * percentageRange) / 100)
+    const minPart = avg - range
+    const maxPart = avg + range
 
     // 生成10个正整数，每个都在平均值的波动范围内
-    let parts: number[] = [];
-    let remainingTotal = total;
+    let parts: number[] = []
+    let remainingTotal = total
     for (let i = 0; i < partsCount - 1; i++) {
-        const partValue = Math.floor(Math.random() * (maxPart - minPart + 1) + minPart);
-        parts.push(partValue);
-        remainingTotal -= partValue;
+      const partValue = Math.floor(
+        Math.random() * (maxPart - minPart + 1) + minPart
+      )
+      parts.push(partValue)
+      remainingTotal -= partValue
     }
 
     // 最后一个数确保总和等于原始值
-    const lastPart = Math.floor(remainingTotal);
-    parts.push(lastPart);
+    const lastPart = Math.floor(remainingTotal)
+    parts.push(lastPart)
 
-    return parts;
-}
+    return parts
+  }
 
-const formModel = reactive({
+  const items = ref(3)
+  const amount = ref<number[]>(initAmount())
+  function initAmount() {
+    return new Array(items.value).fill(0)
+  }
+
+  const handleAdd = () => {
+    items.value += 1
+
+    amount.value.push(0)
+  }
+
+  const handleDelete = (index: number) => {
+    items.value -= 1
+    amount.value.splice(index, 1)
+  }
+
+  // 根据不同金额，合计总数
+  function getTotal(arr: number[]) {
+    return arr
+      .map((item) => Number(item) || 0)
+      .reduce((total, current) => {
+        if (typeof current !== 'number') {
+          throw new Error('Array must contain only numbers')
+        }
+        if (current < 0) {
+          throw new Error('Array must contain only non-negative integers')
+        }
+        return total + current
+      }, 0)
+  }
+
+  const formModel = reactive({
     total: 30000,
     partsCount: 10,
     percentageRange: 25
-})
+  })
 
-// const historyList = ref<any[]>(JSON.parse(localStorage.getItem('history') || '[]') || [])
+  watch(
+    () => amount.value,
+    (newVal) => {
+      formModel.total = getTotal(newVal)
+    },
+    { deep: true, immediate: true }
+  )
 
-const historyList = ref<any[]>([])
-const parts = ref<number[]>([])
+  // 上次历史缓存
+  const historyList = ref<number[]>(
+    JSON.parse(localStorage.getItem('history') || '[]') || []
+  )
+  const checkedRecord = ref(false)
 
-const handleComputed = () => {
+  const parts = ref<number[]>([])
 
-    parts.value = randomSplit2Int(formModel.total, formModel.partsCount, formModel.percentageRange) || []
+  // 展示记录
+  const record = computed(() => {
+    if (historyList.value.length > 0 && checkedRecord.value) {
+      return historyList.value
+    } else {
+      return parts.value
+    }
+  })
 
-    historyList.value.push({
-        total: formModel.total,
-        partsCount: formModel.partsCount,
-        percentageRange: formModel.percentageRange,
-        result: parts.value
-    })
+  const handleComputed = () => {
+    parts.value =
+      randomSplit2Int(
+        formModel.total,
+        formModel.partsCount,
+        formModel.percentageRange
+      ) || []
 
-    localStorage.setItem('history', JSON.stringify(historyList.value))
-}
+    checkedRecord.value = false
 
-
+    localStorage.setItem('history', JSON.stringify(parts.value))
+  }
 </script>
 
 <style></style>
---
+++ src/components/Card/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card flex-col gap-4 bg-#fff mb-4 rounded-4 shadow p-4">
+    <div class="flex justify-between items-center b-b-gray-200 b-b-1">
+      <h5 class="font-bold text-#333">{{ title }}</h5>
+
+      <slot name="header-extra" />
+    </div>
+    <div class="mt-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  interface CardProps {
+    title: string
+    width?: number
+  }
+
+  const props = defineProps<CardProps>()
+
+  const width = computed(() => {
+    if (props.width) {
+      return props.width + 'px'
+    } else {
+      return '100%'
+    }
+  })
+</script>
+
+<style scoped>
+  .card {
+    width: v-bind(width);
+  }
+</style>
